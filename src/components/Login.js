616b4df0c3a2363ac44ab4ff2be60812d545b726
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-30 12:58:07 -0500

    Manage state using Immer

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { Grid, Header, Segment, Form, Button } from "semantic-ui-react";
 
-function Login() {
+function Login({user:{username, mobileNumber}, setUser}) {
+  // draft => coing from immer
+  // event => React's synthetic event
+  // data => Since semantic ui is used here
+  function populateFields(event, data) {
+    setUser((draft) => {
+      draft[data.name] = data.value
+    })
+  }
+
   return (
     <Grid textAlign="center" verticalAlign="middle" style={{ height: "100vh" }}>
       <Grid.Column style={{ maxWidth: 450 }}>
@@ -16,12 +25,19 @@ function Login() {
               icon="user"
               iconPosition="left"
               placeHolder="Username"
-            />
+              value={username}
+              onChange={(event,data) => populateFields(event,data)}
+              name="username"
+
+              />
             <Form.Input
               fluid
               icon="mobile alternate"
               iconPosition="left"
               placeholder="Mobile number"
+              value={mobileNumber}
+              onChange={(event,data) => populateFields(event,data)}
+              name="mobileNumber"
             />
             <Button color="blue" fluid size="large">
               Login/Signup
