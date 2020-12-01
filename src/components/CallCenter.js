72000ef9a1a80dd38e8770749c9a91b038f29185
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-12-01 16:38:49 -0500

    Set up call progress bar

--- src/components/CallCenter.js
+++ src/components/CallCenter.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar';
+import CallProgress from './CallProgress';
 
 function CallCenter() {
   return (
     <div>
     <NavBar/>
+    <CallProgress/>
     </div>
   )
 }
